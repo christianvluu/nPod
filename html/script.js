598a100259c2ec6379d5b75849db18d721bfecf4
spacemen13579 <35637788+spacemen13579@users.noreply.github.com>
2019-04-23 17:54:56 -0400

    added button to use the init token provided to call parameter from arduino

--- html/script.js
+++ html/script.js
@@ -13,8 +13,21 @@ $.post("https://api.thinger.io/oauth/token", loginParams)
 .done( data => {
   console.log(data);
   access_token = data.access_token;
+  /*$.get("https://api.thinger.io/v1/users/supernick125/devices/slave_1/millis()", {authorization: access_token})
+    .done(data => document.write(data))//console.log(data))
+    .fail(data => console.log("failure", data))*/
+})
+.fail( data => console.log(data))
+
+
+var button = document.createElement("testButton");
+button.innerHTML = "Button";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
   $.get("https://api.thinger.io/v1/users/supernick125/devices/slave_1/millis()", {authorization: access_token})
     .done(data => document.write(data))//console.log(data))
     .fail(data => console.log("failure", data))
-})
-.fail( data => console.log(data))
+});
