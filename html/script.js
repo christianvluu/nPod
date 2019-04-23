35968b35220bbbfa13f006501c87ca247743c30c
supernick125 <nickhealy33@gmail.com>
2019-04-23 16:39:15 -0400

    changed output to document

--- html/script.js
+++ html/script.js
@@ -1,19 +1,20 @@
 
-var username = "";
-var password = "";
+var username = "supernick125";
+var password = "nPod2019";
 
-var myObject = {
+var loginParams = {
   grant_type: "password",
   username: username,
   password: password
 }
+
 var access_token = "";
-$.post("https://api.thinger.io/oauth/token", myObject)
+$.post("https://api.thinger.io/oauth/token", loginParams)
 .done( data => {
   console.log(data);
   access_token = data.access_token;
   $.get("https://api.thinger.io/v1/users/supernick125/devices/slave_1/millis()", {authorization: access_token})
-    .done(data => console.log(data))
+    .done(data => document.write(data))//console.log(data))
     .fail(data => console.log("failure", data))
 })
 .fail( data => console.log(data))
