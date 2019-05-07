114dd7a1c6885e9445a41e092cceb5a2cdd50633
supernick125 <nickhealy33@gmail.com>
2019-05-07 19:55:18 -0400

    Finished getFloorMotion()

--- html/script.js
+++ html/script.js
@@ -1,19 +1,20 @@
 
 //shortened pod names
 const podsB1 = ["LIB_FLRB1_POD_0","LIB_FLRB1_POD_1", "LIB_FLRB1_POD_2"];
-const pods0 = ["LIB_FLR0_POD_0", "LIB_FLR0_POD_1", "LIB_FLR0_POD_2", "LIB_FLR0_POD_3"];
-const pods1 = ["LIB_FLR1_POD_0", "LIB_FLR1_POD_1", "LIB_FLR1_POD_2", "LIB_FLR1_POD_3"];
+const podsF0 = ["LIB_FLR0_POD_0", "LIB_FLR0_POD_1", "LIB_FLR0_POD_2", "LIB_FLR0_POD_3"];
+const podsF1 = ["LIB_FLR1_POD_0", "LIB_FLR1_POD_1", "LIB_FLR1_POD_2", "LIB_FLR1_POD_3"];
 
 //resource variables
 const millis = "millis";
 const isMotion = "isMotion";
 
-//how many pods on floor
+//floor placeholders
 const floorB1 = -1; //basement
 const floor0 = 0; //ground floor
 const floor1 = 1; //second floor
 
 
+//login parameters for thinger.io
 var username ="npod"; //password in external file
 var loginParams = {
   grant_type: "password",
@@ -21,6 +22,7 @@ var loginParams = {
   password: password
 }
 
+//gets access token from thinger.io and stores in access_token
 var access_token = "";
 $.post("https://api.thinger.io/oauth/token", loginParams)
 .done( data => {
@@ -30,38 +32,34 @@ $.post("https://api.thinger.io/oauth/token", loginParams)
 .fail( data => console.log(data))
 
 
-/*function getFloorTest() {
-  for(i = 0; i < 2; i++) {
-    var id = isMotion + i;
-    getPod(pods[i], isMotion, id);
-  }
-}*/
-
-function getFloor(floor) { //not done
-  switch(floor) {
-    case -1:
-      for(i = 0; i < 3; i++) {
-        var id = B1pod + i;
-        getPod(podsB1[i], isMotion, id);
-      }
-    case 0:
-      for(i = 0; i < 3; i++) {
-        var id = pod + i;
-        getPod(podsB1[i], isMotion, id);
-      }
-    case 1:
-      for(i = 0; i < 3; i++) {
-        var id = pod + i;
-        getPod(podsB1[i], isMotion, id);
-      }
-    default:
-      console.log("Error - floor not valid");
-  }
-}
-
+//gets a resource from a single pod and fills an html element with it
+//device name, resource to get, HTML id value
 function getPod(device, resource, id) {
   var url = "https://api.thinger.io/v1/users/npod/devices/" + device + "/" + resource;
   $.get(url, {authorization: access_token})
     .done(data => document.getElementById(id).innerHTML = data)
     .fail(data => console.log("failure", data))
+}
+
+//gets isMotion values for an entire floor
+function getFloorMotion(floor) {
+  switch(floor) {
+    case -1: //basement
+      for(i = 0; i < 2; i++) { //2 or 3 in basement?
+        var id = B1pod + i;
+        getPod(podsB1[i], isMotion, id);
+      }
+    case 0: //ground floor
+      for(i = 0; i < 4; i++) {
+        var id = F0pod + i;
+        getPod(podsF1[i], isMotion, id);
+      }
+    case 1: //second floor
+      for(i = 0; i < 4; i++) {
+        var id = F1pod + i;
+        getPod(podsF1[i], isMotion, id);
+      }
+    default: //invalid parameter
+      console.log("Error - floor not valid");
+  }
 }
